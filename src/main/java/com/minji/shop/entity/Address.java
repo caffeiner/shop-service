6e19c93e361d8e1b09ca252d17a4c112b6f668aa
권민지 <kmj9247@naver.com>
2023-01-10 22:31:56 +0900

    add embeddable type class Address

--- src/main/java/com/minji/shop/entity/Address.java
+++ src/main/java/com/minji/shop/entity/Address.java
@@ -11,12 +11,16 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
     protected Address() {
+        /* JPA 스펙상
+         엔티티나 임베디드 타입( @Embeddable )은 자바 기본 생성자(default constructor)를
+         public 또는 protected 로 설정해야 한다 */
     }
+
     public Address(String city, String street, String zipcode) {
         this.city = city;
         this.street = street;
         this.zipcode = zipcode;
     }
-
 }
